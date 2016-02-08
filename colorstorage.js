1d05752bb1bcb45acb742883807eccfae249ad40
Kasia Kordys <kasia.kordys@gmail.com>
2016-02-08 22:53:34 +0000

    The two html files share the same localStorage for the background color

--- colorstorage.js
+++ colorstorage.js
@@ -1,18 +1,17 @@
 
 function myFunction() {
-	
-	if (localStorage.bgColor.length !== 0) {
-		
-		document.body.style.backgroundColor = localStorage.bgColor;
-		
-	}
-	
-	else {
 		
 		var x = document.getElementById("myColor").value;
 		document.getElementById("demo").innerHTML = x;
 		document.body.style.backgroundColor = x;
-	}
+		localStorage.bgColor = x;
+			}
     
+function init () {
 
+if (localStorage.bgColor.length !== 0) {
+		document.body.style.backgroundColor = localStorage.bgColor;
 }
+}
+
+
