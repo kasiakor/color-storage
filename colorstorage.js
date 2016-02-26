e32b667fb601e90a2e62d06754abd97c27c89de2
Kasia Kordys <kasia.kordys@gmail.com>
2016-02-26 21:32:19 +0000

    Html onclick event attribute moved to js file

--- colorstorage.js
+++ colorstorage.js
@@ -1,16 +1,23 @@
-
 function setBackground () {				
 	var x = document.getElementById("myColor").value;
 	localStorage.bgColor = x;
 	document.body.style.backgroundColor = x;
 	document.getElementById("foo").innerHTML = x;
+	
+
 				
 };
-				
+
+document.getElementById("myBtn").setAttribute( "onclick", "setBackground()");	
 				
 function loadBackground () {
 	if (localStorage.bgColor.length !== 0) {
 		document.body.style.backgroundColor = localStorage.bgColor;
-		document.getElementById("myColor").value = localStorage.bgColor;			
+		document.getElementById("myColor").value = localStorage.bgColor;
+
 	}
 };
+
+
+
+	
